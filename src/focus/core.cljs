8eaaa7b4086a42ea9a3ddbd929e7fbe46fdab58e
Zekna <zsharpsquared@gmail.com>
2014-06-24 23:26:30 -0700

    Working and pretty.

--- src/focus/core.cljs
+++ src/focus/core.cljs
@@ -13,6 +13,26 @@
 
 (enable-console-print!)
 
+;; styles
+
+(def incremental-btn
+  [:.incremental-btn {:margin-left "-30px"
+                      :fuck-you-left "99999999px"
+                      :margin-top "25px"
+                      }])
+
+(def styles (css (concat
+                   [[:body
+                     {:background "#EEE"}
+                     ]]
+                   [[:.wrapper {:width "100%"
+                                :padding "20px"}]]
+                   [incremental-btn])))
+
+(cssom/addCssText styles)
+
+;; app logic and logic components
+
 (def app-state (atom {:lapses 0 :breaks 0 :ratio 3 :last-break 0}))
 
 (defn increment-state [data edit-key owner]
@@ -30,23 +50,23 @@
 
 (defn incremental-text [app owner]
   (reify
-    om/IInitState
-    (init-state [this]
-      )
     om/IRenderState
     (render-state [this {:keys [label num-key]}]
-      (html [:div
-             [:label label]
-             [:input {:type "text" :value (num-key app)}]
-             [:button {:on-click #(increment-state app num-key this)} "+"]]))))
+      (html [:div 
+             [:div {:class "form-group col-xs-3"}
+               [:label label]
+               [:input {:type "text" :value (num-key app) :class "form-control"}]]
+             [:div {:class "col-xs-1"}
+               [:button {:class "btn incremental-btn" 
+                         :on-click #(increment-state app num-key this)} "+"]]]))))
 
 (defn editable [app owner]
   (reify
     om/IRenderState
     (render-state [this {:keys [label val-key]}]
-      (html [:div
+      (html [:div {:class "form-group col-xs-4"}
              [:label label]
-             [:input {:type "text" :value (val-key app) 
+             [:input {:type "text" :value (val-key app) :class "form-control"
                       :on-change #(handle-numeric-change % app val-key owner)}]]))))
 
 (defn last-break [app owner]
@@ -54,40 +74,48 @@
     om/IRender
     (render [this]
       (html
-        [:div "Lapses since last break: "
+        [:div
+         [:b "Lapses since last break: "]
          [:span (- (:lapses app) (:last-break app))]]))))
 
-;; Summary messages about performance overall -- not super necessary
+;; Summary messages about performance overall -- rather unnecessary
 (defn ratio-check [app owner]
   (reify
     om/IRender
     (render [this]
       (html [:div
+             [:b "Overall: "]
              (if (>= (:lapses app) (* (:breaks app) (:ratio app)))
-               [:div "Overall: you're doing well!"]
-               [:div "Overall: try to focus a bit more next time!"])]))))
+               [:p {:class "text-success"} "You're doing well!"]
+               [:p {:class "text-danger"} "Try to focus a bit more next time!"])]))))
 
 (defn current-check [app owner]
   (reify
     om/IRender
     (render [this]
       (html [:div
+             [:b "Currently: "]
              (if (>= (- (:lapses app) (:last-break app)) (:ratio app))
-               [:div "Currently: Feel free to take a short break anytime!"]
-               [:div "Currently: Keep going! You can do it!"])]))))
+               [:p {:class "text-success"} "Feel free to take a short break anytime!"]
+               [:p "Keep going! You can do it!"])]))))
 
 (defn focus-view [app owner]
   (reify
     om/IRender
     (render [this]
-      (html [:div
-             (om/build editable app {:init-state {:label "Ratio:" :val-key :ratio}})
-             (map #(om/build incremental-text app {:init-state %})
-                   [{:label "Lapses:" :num-key :lapses}
-                    {:label "Breaks:" :num-key :breaks}])
-             (om/build last-break app)
-             (om/build current-check app)
-             (om/build ratio-check app)]))))
+      (html [:div {:class "wrapper"}
+             [:div {:class "row"}
+               (om/build editable app {:init-state {:label "Ratio:" :val-key :ratio}})
+               (map #(om/build incremental-text app {:init-state %})
+                     [{:label "Lapses:" :num-key :lapses}
+                      {:label "Breaks:" :num-key :breaks}])]
+             [:div {:class "row"}
+              [:div {:class "col-xs-3"}
+               (om/build last-break app)]
+              [:div {:class "col-xs-3"}
+               (om/build current-check app)]
+              [:div {:class "col-xs-3"}
+               (om/build ratio-check app)]]]))))
 
 (om/root focus-view app-state
   {:target (.getElementById js/document "focus")})
